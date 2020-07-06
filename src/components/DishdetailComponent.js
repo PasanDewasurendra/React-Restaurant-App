2de8d8db92efc48a081f8e092e579d044a459fb1
PasanDewasurendra <pasandewasurendra@gmail.com>
2020-07-06 15:51:35 +0530

    set fetching data from json-server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -3,9 +3,11 @@ import { CardImg, CardBody, Card, CardTitle, CardText, Breadcrumb, BreadcrumbIte
 import { Link } from 'react-router-dom'
 import { LocalForm, Control, Errors } from 'react-redux-form'
 import { Loading } from './LoadingComponent'
+import { baseUrl } from '../shared/baseUrl'
 const required = (val) => val && val.length;
 const maxLength = (len) => (val) => !(val) || (val.length <= len);
 const minLength = (len) => (val) => (val) && (val.length >= len);
+
 export class CommentForm extends Component{
 
     constructor(props) {
@@ -93,7 +95,7 @@ export class CommentForm extends Component{
 function RenderDish({dish}){
     return(
         <Card className="col-12 col-md-5 m-1">
-            <CardImg width="100%" src={dish.image} alt={dish.name} />
+            <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
             <CardBody>
                 <CardTitle>{dish.name}</CardTitle>
                 <CardText>{dish.description}</CardText>
